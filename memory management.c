760c7f6ec6e989cebd411ebe4e9fcf3b8be49d80
Pponghub <wallop.wwww@gmail.com>
2023-08-11 22:14:44 +0700

    global,malloc check

--- memory management.c
+++ memory management.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int x,y,z;
+
+int main(){
+    printf("Address of x: %p\n", &x);
+    printf("Address of y: %p\n", &y);
+    printf("Address of z: %p\n", &z);
+
+    int* A = (int*)malloc(100*sizeof(int));
+    int* B = (int*)malloc(100*sizeof(int));
+    int* C = (int*)malloc(100*sizeof(int));
+
+    printf("Address of A: %p\n", &A);
+    printf("Address of B: %p\n", &B);
+    printf("Address of C: %p\n", &C);
+
+    return 0;
+}
